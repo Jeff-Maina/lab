68dc4b0db07f6fb355e5945a548556795d68b625
Jeff-Maina <devyoungblud@gmail.com>
2024-03-02 23:00:53 +0300

    Initialize repo

---
+++ src/app/components/homepage/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className="p-6 text-sm text-zinc-600 max-w-3xl m-auto flex items-center justify-between">
+      <p>井底之蛙</p>
+      <p>
+        Made by{" "}
+        <a href="" className="text-black hover:underline">
+          Jeff
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/app/data/Appdata.ts
@@ -0,0 +1,14 @@
+type ProjectProps = {
+    project: string,
+    dateCreated: string,
+    draft: boolean,
+    complete: boolean,
+}
+
+export const ProjectsList: Array<ProjectProps> = [{
+    project: "Oppo's password input",
+    dateCreated: "Saturday, 2 March 2024",
+    draft: false,
+    complete: true
+}
+]
